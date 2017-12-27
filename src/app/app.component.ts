ed18b6f695944308dcdffd76af43407864ad78eb
Maleja <alejandra.giraldo.consultant@sothebys.com>
2017-12-27 11:04:32 -0500

    Update Dependency Of Auto-Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, HostListener, Inject } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { CompleterService, CompleterData } from 'ng2-completer';
 import { Subscription } from 'rxjs/Subscription';
 import { DOCUMENT } from '@angular/platform-browser';
+import { CreateNewAutocompleteGroup, SelectedAutocompleteItem, NgAutocompleteComponent } from 'ng-auto-complete';
+
 
 @Component({
   selector: 'app-root',
@@ -9,40 +11,33 @@ import { DOCUMENT } from '@angular/platform-browser';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  protected searchStr: string;
-  protected captain: string;
-  protected dataService: CompleterData;
-  protected searchData = [
-    { color: 'red', value: '#f00' },
-    { color: 'green', value: '#0f0' },
-    { color: 'blue', value: '#00f' },
-    { color: 'cyan', value: '#0ff' },
-    { color: 'magenta', value: '#f0f' },
-    { color: 'yellow', value: '#ff0' },
-    { color: 'black', value: '#000' }
+  @ViewChild(NgAutocompleteComponent) public completer: NgAutocompleteComponent;
+
+  public group = [
+    CreateNewAutocompleteGroup(
+      'Search / choose in / from list',
+      'completer',
+      [
+        { title: 'Option 1', id: '1' },
+        { title: 'Option 2', id: '2' },
+        { title: 'Option 3', id: '3' },
+        { title: 'Option 4', id: '4' },
+        { title: 'Option 5', id: '5' },
+      ],
+      { titleKey: 'title', childrenKey: null }
+    ),
   ];
-  protected captains = ['James T. Kirk', 'Benjamin Sisko', 'Jean-Luc Picard', 'Spock', 'Jonathan Archer', 'Hikaru Sulu', 'Christopher Pike', 'Rachel Garrett'];
-  public isScrolled = false;
 
-  constructor(
-    @Inject(DOCUMENT) private document: any,
-    private completerService: CompleterService) {
-    this.dataService = completerService.local(this.searchData, 'color', 'color');
+  constructor() {
+
   }
-  @HostListener('document:scroll', ['$event'])
-  onWindowScroll($event) {
-    let count = 0;
-    count += 1;
-    debugger
-    console.log('Scrolling', count);
 
-    const number = this.document.body.scrollTop;
-    if (number > 150) {
-
-      this.isScrolled = true;
-    } else if (this.isScrolled && number < 10) {
-
-      this.isScrolled = false;
-    }
+  /**
+   *
+   * @param item
+   * @constructor
+   */
+  Selected(item: SelectedAutocompleteItem) {
+    console.log(item);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Ng2CompleterModule } from './../../node_modules/ng2-completer/ng2-completer.module';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import {NgAutoCompleteModule} from 'ng-auto-complete';
 
 
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     Ng2CompleterModule,
     FormsModule,
-    InfiniteScrollModule
+    InfiniteScrollModule,
+    NgAutoCompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
